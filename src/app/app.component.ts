df649a884c16ea56b2793ee305dacfe9df124860
thaua-engelmann <engelmann.webdev@gmail.com>
2025-03-26 19:28:13 -0300

    feat(header): create component and basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { HeaderComponent } from './ui/header/header.component';
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { RegisterIconsService } from 'src/app/services/register-icons/register-icons.service';
+
+type NavbarItem = {id: number; text: string};
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+
+  private registerIconsService = inject(RegisterIconsService);
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: string
+  ) {
+      this.registerIconsService.register(['search']);
+  }
+
+  protected readonly _navbarItems: NavbarItem[] = [
+    {
+      id: 1,
+      text: "Home"
+    },
+    {
+      id: 2,
+      text: "Contact"
+    },
+    {
+      id: 3,
+      text: "About"
+    },
+    {
+      id: 4,
+      text: "Sign Up"
+    },
+  ]
+
+  ngOnInit(): void {
+    const teste = ''
+    teste.replace('', '');
+  }
+
+}
