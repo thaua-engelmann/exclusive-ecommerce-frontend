52b6331487ab6ca507addd4c2c2b23bad2c00f54
thaua-engelmann <engelmann.webdev@gmail.com>
2025-04-06 13:21:45 -0300

    config(routing): set up default /sign-up route as default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'sign-up'
+    },
+    {
+        path: 'sign-up',
+        loadComponent: () => import('./pages/sign-up/sign-up.component').then(c => c.SignUpComponent)
+    }
+];
