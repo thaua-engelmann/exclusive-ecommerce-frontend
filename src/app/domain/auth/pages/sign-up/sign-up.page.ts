42dbeb7fa89c34a573dd9aae2837075c880aa35d
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-04-21 09:57:21 -0300

    ✨ feat(sign-up): create the inputs and animate a floating label to each input

--- src/app/domain/auth/pages/sign-up/sign-up.page.ts
+++ src/app/domain/auth/pages/sign-up/sign-up.page.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+type InputField = {
+  id: number;
+  label: string;
+  isFocused?: boolean;
+};
+
 @Component({
   selector: 'app-sign-up',
   imports: [CommonModule],
   templateUrl: './sign-up.page.html',
   styleUrl: './sign-up.page.scss',
 })
-export class SignUpPage {}
+export class SignUpPage {
+  protected readonly _fields: InputField[] = [
+    {
+      id: 1,
+      label: 'Name',
+    },
+    {
+      id: 2,
+      label: 'E-mail or Phone Number',
+    },
+    {
+      id: 3,
+      label: 'Password',
+    },
+  ];
+
+  protected onFocusInput = (field: InputField): void => {
+    field.isFocused = true;
+  };
+
+  protected onBlurInput = (field: InputField): void => {
+    field.isFocused = false;
+  };
+}
