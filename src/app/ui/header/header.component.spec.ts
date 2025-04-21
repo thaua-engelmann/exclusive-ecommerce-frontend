ba7febfe6e3f467fcb1252509bd0d403569a0ef9
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-04-21 12:20:40 -0300

    🧪 test(header): Check logo text to be equal

--- src/app/ui/header/header.component.spec.ts
+++ src/app/ui/header/header.component.spec.ts
@@ -1,21 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HeaderComponent],
+      providers: [provideHttpClient()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render the logo text as "Exclusive"', () => {
+    const debugEl: HTMLHeadingElement = fixture.debugElement.query(
+      By.css('h1'),
+    ).nativeElement;
+    expect(debugEl.textContent).toBe('Exclusive');
   });
 });
