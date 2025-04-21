8cb91e5404c2d73055f3ca049860a2e53486024d
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-04-21 15:04:52 -0300

    🧪 test(app): create MockHeaderComponent to test its rendering

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,34 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { By } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './ui/header/header.component';
+
+import { MockHeaderComponent } from './../testing/mocks/header.mock';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterModule.forRoot([])],
+      imports: [AppComponent, RouterModule.forRoot([])],
       providers: [provideHttpClient()],
     }).compileComponents();
   });
 
-  it('should render title', () => {
+  TestBed.overrideComponent(AppComponent, {
+    remove: {
+      imports: [HeaderComponent],
+    },
+    add: {
+      imports: [MockHeaderComponent],
+    },
+  });
+
+  it('should render header component', () => {
     const fixture = TestBed.createComponent(AppComponent);
+
+    const debugEl = fixture.debugElement.query(By.css('app-header'));
+    expect(debugEl).toBeTruthy();
   });
 });
